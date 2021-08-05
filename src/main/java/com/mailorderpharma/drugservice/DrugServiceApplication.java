2fb60dac914dada6635d3c80ada9d48540d7baa6
KAMINWAR SAI PRIYA <k.saipriya08@gmail.com>
2021-08-05 11:56:38 +0530

    modified pom

--- src/main/java/com/mailorderpharma/drugservice/DrugServiceApplication.java
+++ src/main/java/com/mailorderpharma/drugservice/DrugServiceApplication.java
@@ -2,11 +2,12 @@ package com.mailorderpharma.drugservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class DrugServiceApplication {
+public class DrugServiceApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DrugServiceApplication.class, args);
